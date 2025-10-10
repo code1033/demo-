4d47e49f18e5830e01d350bc147f622d231616b3
omar alabbas <12502069@student.pxl.be>
2025-10-10 17:10:25 +0200

    Add test3.cs

---
+++ test3.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace H
+{
+    class J
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("write a number :");
+            int n = Convert.ToInt16(Console.ReadLine());
+
+            if (n == 5)
+            {
+                Console.WriteLine("you win");
+            }
+            else
+            {
+                Console.WriteLine("you lost");
+            }
+            }
+        }
+    }
+}
